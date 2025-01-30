f19c253a7968ce660c7ee54caa47b7d6e3b79b92
Luciano Gasparin <lucianogasparin57@gmail.com>
2025-01-30 00:20:29 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+console.log(history);
+console.log(window.document.URL);
+const arrowLeft = document.getElementById('arrow-left');
+const arrowRight = document.getElementById('arrow-right');
+const meetingButton = document.getElementById('meetingButton');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
+const text4 = document.getElementById('text4');
+const text5 = document.getElementById('text5');
+const video1 = document.getElementById('video1');
+const video2 = document.getElementById('video2');
+const video3 = document.getElementById('video3');
+const video4 = document.getElementById('video4');
+const video5 = document.getElementById('video5');
+
+const overlayContent = [
+    { text: text1, video: video1 },
+    { text: text2, video: video2 },
+    { text: text3, video: video3 },
+    { text: text4, video: video4 },
+    { text: text5, video: video5 }
+]
+
+var counter = 0;
+
+arrowRight.addEventListener('click', () => {
+    counter++;
+    let currentText = overlayContent[counter - 1].text;
+    let nextText = overlayContent[counter].text;
+    let currentVideo = overlayContent[counter - 1].video;
+    let nextVideo = overlayContent[counter].video;
+    currentText.style = 'display: none';
+    currentVideo.style = 'display: none';
+    nextText.style = 'display: flex';
+    nextVideo.style = 'display: flex';
+    if (counter >= 1) {
+        arrowLeft.style = 'visibility: visible';
+    }
+    if (counter === overlayContent.length - 1) {
+        arrowRight.style = 'visibility: hidden';
+        meetingButton.style = 'visibility: visible';
+    }
+})
+
+arrowLeft.addEventListener('click', () => {
+    counter--;
+    let currentText = overlayContent[counter + 1].text;
+    let nextText = overlayContent[counter].text;
+    let currentVideo = overlayContent[counter + 1].video;
+    let nextVideo = overlayContent[counter].video;
+    currentText.style = 'display: none';
+    currentVideo.style = 'display: none';
+    nextText.style = 'display: flex';
+    nextVideo.style = 'display: flex';
+    if (counter <= overlayContent.length - 1) {
+        arrowRight.style = 'visibility: visible';
+    }
+    if (counter === 0) {
+        arrowLeft.style = 'visibility: hidden';
+    }
+})
