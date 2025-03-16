5fb2b6d08c35894029e09fe4f5ebdf42c2f0caef
Luciano Gasparin <lucianogasparin5721@gmail.com>
2025-03-15 21:52:37 -0300

    Movendo .git para pasta ../StylegramRepository

--- Stylegram/pages/nossa-comunidade/nossa-comunidade.js
+++ Stylegram/pages/nossa-comunidade/nossa-comunidade.js
@@ -4,9 +4,9 @@ export default async function () {
         const response = await fetch('pages/nossa-comunidade/nossa-comunidade.html');
         const html = await response.text();
 
-        const section = document.createElement('section');
-        section.innerHTML = html;
-        document.body.appendChild(section);
+        const div = document.createElement('div');
+        div.innerHTML = html;
+        document.body.appendChild(div);
     } catch (error) {
         console.error(`Não foi possível carregar a section: ${error}`);
     }
